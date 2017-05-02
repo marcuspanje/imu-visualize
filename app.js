5f58494dafccc6c5ba7330c472b3bc534d46d81b
marcuspanje <marcuspanje@gmail.com>
2017-05-02 15:45:20 -0700

    viz my imu

--- app.js
+++ app.js
@@ -1,73 +1,29 @@
-var app = require('http').createServer(handler);
-var url= require('url');
-var fs = require('fs');
-var os = require('os');
-var io = require('socket.io').listen(app);
-var serialport = require("serialport");
-var SP = serialport.SerialPort;
-var serialPort = new SP("COM4",
+var server = require('http').createServer();
+var io = require('socket.io').listen(server);
+server.listen(5000);
+var SerialPort = require("serialport");
+var sp = new SerialPort("/dev/tty.usbmodem1422",
 	{
-		baudrate: 115200,
-		parser: serialport.parsers.readline("\n")
-	}, false);
+		baudrate: 9600,
+		parser: SerialPort.parsers.readline("\n"),
+    autoOpen: false
+	});
 
-app.listen(5000);
+
 
 /* SERIAL WORK */
 
-serialPort.open(function (error) {
+sp.open(function (error) {
   if ( error ) {
     console.log('failed to open: '+error);
   } else {
     console.log('open');
-    serialPort.on('data', function(data) {
+    sp.on('data', function(data) {
       console.log('data received: ' + data);
       io.sockets.emit('serial_update', data);
     });
-    //serialPort.write("ls\n", function(err, results) {
-    //  console.log('err ' + err);
-    //  console.log('results ' + results);
-    //});
   }
 });
 
 
-// Http handler function
-function handler (req, res) {
-    
-    // Using URL to parse the requested URL
-    var path = url.parse(req.url).pathname;
-    
-    // Managing the root route
-    if (path == '/') {
-        index = fs.readFile(__dirname+'/three.html', 
-            function(error,data) {
-                
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load three.html");
-                }
-                
-                res.writeHead(200,{'Content-Type': 'text/html'});
-                res.end(data);
-            });
 
-    // Managing the route for the javascript files
-    } else if( /\.(js)$/.test(path) ) {
-        index = fs.readFile(__dirname+path, 
-            function(error,data) {
-                
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load " + path);
-                }
-                
-                res.writeHead(200,{'Content-Type': 'text/plain'});
-                res.end(data);
-            });
-    } else {
-        res.writeHead(404);
-        res.end("Error: 404 - File not found.");
-    }
-    
-}
--- generateThree.js
+++ generateThree.js
@@ -7,117 +7,199 @@ Generate 3D render using serial data from IMU
 'use strict';
 
 // Declare required variables
-var dataRollx = 0;
-var dataRolly = 0;
-var dataRollz = 0;
-var dataRollxArray = [];
-var dataRollyArray = [];
-var dataRollzArray = [];
+var poseArray = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+var positionRaw = new THREE.Vector3(0.0);
+var rotationRaw = new THREE.Vector3(0.0);
+var position = new THREE.Vector3(0.0);
+var rotation = new THREE.Vector3(0.0);
 var accuracy = 2;
 var orderOfMag = (Math.PI/180);
 var container;
 var camera, scene, renderer;
-var cube, plane;
+var geometry_lines;
+var probe_axes, plane;
 var targetRotation = 0;
 var targetRotationOnMouseDown = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
+var px_cm = 20;//no of px in 1 cm 
+var dim = 3; //2D or 3D
+var gridRes = 0.5*px_cm; //cm
+var gridSize = 20*px_cm;
+var controls;
+var euler = new THREE.Euler( Math.PI/2, 0, Math.PI/2, 'XYZ' );
+var imuToVizRot = new THREE.Matrix4().makeRotationFromEuler(euler);
+var degToRad = Math.PI/180;
 
 //Connect to socket.io
+
 var serverIP = "localhost";
-var socket = io.connect(serverIP + ':5000');
-console.log('socket connected to: ' + serverIP);
+var port = 5000
+var socket = io('http://' + serverIP + ':' + port +  '/');
 
 // Start reading IMU data
 runSocket();
 init();
 animate();
 
+
 function runSocket() {
-        socket.on('serial_update', function(data) {
-            if (data.charAt(0) === 'O') {
-                console.log(data);
-                var dataArray = data.split(/ /);
+  socket.on('serial_update', function(data) {
+    //console.log(data);
+    var dataArray = data.split(" ");
+    if (dataArray[0] == "error:") {
+        return;
+    }
+    positionRaw.x = parseFloat(dataArray[0]);
+    positionRaw.y = parseFloat(dataArray[1]);
+    positionRaw.z = parseFloat(dataArray[2]);
+    rotationRaw.x = parseFloat(dataArray[3]);
+    rotationRaw.y = parseFloat(dataArray[4]);
+    rotationRaw.z = parseFloat(dataArray[5]);
+    
+  });
+}
 
-                // set x
-                dataRollx = (dataArray[1] *= orderOfMag).toFixed(accuracy);
-                
-                // set y
-                dataRolly = (dataArray[2] *= orderOfMag).toFixed(accuracy);
+function buildAxis( src, dst, colorHex, dashed ) {
+  var geom = new THREE.Geometry(), mat; 
 
-                // set z
-                dataRollz = (dataArray[3] *= orderOfMag).toFixed(accuracy);
+  if(dashed) {
+    mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
+  } else {
+    mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
+  }
 
-                console.log(dataRollx + "," + dataRolly + "," + dataRollz);
-            }
-        });
+  geom.vertices.push( src.clone() );
+  geom.vertices.push( dst.clone() );
+  geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
+  var axis = new THREE.Line( geom, mat, THREE.LinePieces );
+  return axis;
+
 }
 
 function init() {
+  container = document.createElement( 'div' );
+  document.body.appendChild( container );
 
-    container = document.createElement( 'div' );
-    document.body.appendChild( container );
+  var info = document.createElement( 'div' );
+  info.style.position = 'absolute';
+  info.style.top = '10px';
+  info.style.width = '100%';
+  info.style.textAlign = 'center';
+  info.innerHTML = 'Visualize IMU';
+  info.setAttribute('id', 'pourHeading');
+  container.appendChild( info );
 
-    var info = document.createElement( 'div' );
-    info.style.position = 'absolute';
-    info.style.top = '10px';
-    info.style.width = '100%';
-    info.style.textAlign = 'center';
-    info.innerHTML = 'Visualize IMU';
-    info.setAttribute('id', 'pourHeading');
-    container.appendChild( info );
+  var poseVals = document.createElement('div');
+  poseVals.style.position = 'absolute';
+  poseVals.style.top = '10px';
+  poseVals.style.width = '100%';
+  poseVals.style.textAlign = 'center';
+  poseVals.innerHTML = '';
+  poseVals.setAttribute('id', 'poseVals');
 
-    $("#pourHeading").append("<div id='subHeading'></div>");
+  container.appendChild( poseVals );
 
-    // Set up camera
+  $("#pourHeading").append("<div id='subHeading'></div>");
+  $("#poseVals").append("<div id='subHeading'></div>");
+
+
+    //init imuToViz rotation mat
+  
+
+  var grid = new THREE.GridHelper(gridSize, gridSize/gridRes);
+  console.log(grid);
+  
+  // Set up camera looking down the z axis, 100 cm away.
+  if (dim == 3) {
+
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.y = 150;
-    camera.position.z = 500;
+    camera.position.set(0*px_cm, 10*px_cm, 10*px_cm);
+    camera.lookAt(new THREE.Vector3(0,0,0));
 
-    scene = new THREE.Scene();
+  } else if (dim == 2) {
+    var lim = 10 * px_cm;
+    camera = new THREE.OrthographicCamera(-lim, lim, lim, -lim, 1*px_cm, 100*px_cm);
+    camera.position.set(0,0,10*px_cm);
+    camera.up = new THREE.Vector3(0,1,0);
+    camera.lookAt(new THREE.Vector3(0,0,0));
 
-    // Create cube
-    var geometry = new THREE.BoxGeometry( 200, 200, 200 );
+    grid.rotation.x = Math.PI/2;
 
-    for ( var i = 0; i < geometry.faces.length; i += 2 ) {
+    console.log(camera.position);
+  }
 
-        var hex = Math.random() * 0xffffff;
-        geometry.faces[ i ].color.setHex( hex );
-        geometry.faces[ i + 1 ].color.setHex( hex );
+  scene = new THREE.Scene();
 
-    }
+  // Create probe_axes
 
-    var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors, overdraw: 0.5 } );
+  var length = 1 * px_cm; 
+  
+  probe_axes = new THREE.Object3D();
+  probe_axes.add(buildAxis(new THREE.Vector3(0,0,0),
+    new THREE.Vector3(length,0,0),0xFF0000, false)); // +X
+  probe_axes.add(buildAxis(new THREE.Vector3(0,0,0),
+    new THREE.Vector3(0,length,0),0x00FF00, false)); // +Y
+  probe_axes.add(buildAxis(new THREE.Vector3(0,0,0),
+    new THREE.Vector3(0,0,length),0x0000FF, false)); // +Z
 
-    cube = new THREE.Mesh( geometry, material );
-    cube.position.y = 150;
-    scene.add( cube );
+  
+  scene.add(probe_axes);
 
-    // Create background plane
-    var geometry = new THREE.PlaneBufferGeometry( 400, 200 );
-    geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
+  // Create background grid
+  //
+  var cos45 = Math.sqrt(2)/2;
+  //grid.rotation.set(0,0,cos45,cos45);
+  //grid.position.set(-100,0,0);
 
-    var material = new THREE.MeshBasicMaterial( { color: 0xe0e0e0, overdraw: 0.5 } );
+ // grid.rotation.setFromAxisAngle(
+ //   new THREE.Vector3(1,0,0), Math.PI/2);
 
-    plane = new THREE.Mesh( geometry, material );
-    scene.add( plane );
+  scene.add(grid);
 
-    renderer = new THREE.CanvasRenderer();
-    renderer.setClearColor( 0xf0f0f0 );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    container.appendChild( renderer.domElement );
+  //create line array
+  var material = new THREE.LineBasicMaterial({
+  color: 0x0000ff
+});
 
-    window.addEventListener( 'resize', onWindowResize, false );
+  geometry_lines = new THREE.Geometry();
+  geometry_lines.vertices.push(
+    new THREE.Vector3( 0, 0, 0 ),
+    new THREE.Vector3(0,0,0).copy(position)
+  );
+
+  var line = new THREE.Line( geometry_lines, material );
+  scene.add( line );
+
+  controls = new THREE.TrackballControls( camera );
+  controls.rotateSpeed = 1.0;
+  controls.zoomSpeed = 0.2;
+  controls.panSpeed = 0.8;
+
+  controls.noZoom = false;
+  controls.noPan = false;
+
+  controls.staticMoving = true;
+  controls.dynamicDampingFactor = 0.3;
+  
+
+  renderer = new THREE.CanvasRenderer();
+  renderer.setClearColor( 0xf0f0f0 );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  container.appendChild( renderer.domElement );
+
+  window.addEventListener( 'resize', onWindowResize, false );
 }
 
 function onWindowResize() {
-        windowHalfX = window.innerWidth / 2;
-        windowHalfY = window.innerHeight / 2;
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
 
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
+    //camera.aspect = window.innerWidth / window.innerHeight;
+    //camera.updateProjectionMatrix();
 
-        renderer.setSize( window.innerWidth, window.innerHeight );
+    //renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
 function animate() {
@@ -126,8 +208,22 @@ function animate() {
 }
 
 function render() {
-    cube.rotation.x = -dataRollx;
-    cube.rotation.y = -dataRollz;
-    cube.rotation.z = -dataRolly;
+     
+    position.copy(positionRaw);
+    rotation.copy(rotationRaw.multiplyScalar(degToRad).applyMatrix4(imuToVizRot));
+    console.log(rotation);
+    probe_axes.position.copy(position);
+    probe_axes.rotation.copy(rotation);    //geometry_lines.vertices.push(
+     // new THREE.Vector3(0.0).copy(positionRaw));
+    $("#poseVals").html(
+    "<p>Translation: "
+    + position.x.toFixed(2) + ", " 
+    + position.y.toFixed(2) + ", " 
+    + position.z.toFixed(2) + "</p>" 
+    + "<p>Rotation: "
+    + rotation.x.toFixed(2) + ", " 
+    + rotation.y.toFixed(2) + ", " 
+    + rotation.z.toFixed(2) + "</p>" );
+    controls.update();
     renderer.render( scene, camera );
 }
